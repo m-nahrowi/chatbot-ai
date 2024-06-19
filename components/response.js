6d5e72ff097312c9cd1b633ca56a09d0d9707b61
Muhamad Nahrowi <86625224+m-nahrowi@users.noreply.github.com>
2024-06-19 16:16:24 +0700

    Update response.js

--- components/response.js
+++ components/response.js
@@ -4,7 +4,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 import Markdown from "react-native-markdown-display";
 
 const date = new Date();
-const API_KEY = "AIzaSyDxATGNueahse-vpeg3PUXSvs7ycIAssD8";
+const API_KEY = "";
 const genAI = new GoogleGenerativeAI(API_KEY);
 
 export default function Response(props) {
